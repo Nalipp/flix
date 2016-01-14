96bc354a7d452545c2d02442438d45a74f3d0272
Nalipp <nateplipp@gmail.com>
2016-01-15 08:01:37 +0900

    add movie index view

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,4 +1,11 @@
 module MoviesHelper
+  def format_total_gross(movie)
+    if movie.flop?
+      content_tag(:strong, 'Flop!')
+    else
+      number_to_currency(movie.total_gross)
+    end
+  end
 
   def image_for(movie)
     if movie.image_file_name.blank?
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ActiveRecord::Base
 
   RATINGS = %w(G PG PG-13 R NC-17)
 
+  def flop?
+    total_gross.blank? || total_gross < 50000000
+  end
+
 end
