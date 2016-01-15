986fffb85f3d59b8e8bb81147f82a7daecf8d18d
Nalipp <nateplipp@gmail.com>
2016-01-15 20:58:15 +0900

    update movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,6 +11,22 @@ class Movie < ActiveRecord::Base
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :rating, inclusion: { in: RATINGS }
 
+  def self.released
+    where("released_on <= ?", Time.now).order("released_on desc")
+  end
+
+  def self.hits
+    where('total_gross >= 300000000').order('total_gross desc')
+  end
+
+  def self.flops
+    where('total_gross < 10000000').order('total_gross asc')
+  end
+
+  def self.recently_added
+    order('created_at desc').limit(3)
+  end
+
   def flop?
     total_gross.blank? || total_gross < 50000000
   end
